8d91421c2b138a6c111a2381aeaa8be3eece1770
Cole-showers7 <98387486+Cole-showers7@users.noreply.github.com>
2024-05-14 15:36:44 +0100

    Presentation package contains the viewmodel and the screen

--- app/src/main/java/com/example/retrofittut/data/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/retrofittut/data/ProductRepositoryImpl.kt
@@ -12,13 +12,23 @@ class ProductRepositoryImpl(
     override suspend fun getProductList(): Flow<Result<List<Product>>> {
         return flow {
             val productsFromApi= try {
-
+                api.getProductsList()
             }catch (e: IOException){
                 e.printStackTrace()
                 emit(Result.Error(message = "Error loading products"))
+                return@flow
             }catch (e: HttpException){
-
+                e.printStackTrace()
+                emit(Result.Error(message = "Error loading products"))
+                return@flow
+            }catch (e: Exception){
+                e.printStackTrace()
+                emit(Result.Error(message = "Error loading products"))
+                return@flow
             }
+
+            emit(Result.Success(productsFromApi.products))
         }
+
     }
 }
